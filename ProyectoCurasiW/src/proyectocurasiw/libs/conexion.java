7f14d0d4ad383a5409991d28f34d7b92edf68d9d
winston07 <winstoncurasi@gmail.com>
2014-06-26 14:50:51 -0500

    Update conexion.java

--- ProyectoCurasiW/src/proyectocurasiw/libs/conexion.java
+++ ProyectoCurasiW/src/proyectocurasiw/libs/conexion.java
@@ -11,5 +11,20 @@ package proyectocurasiw.libs;
  * @author alum.fial8
  */
 public class conexion {
+    private static Connection conex=null;
+    private static String url="jdbc:mysql://localhost/bd_visitas";
+    private static String usuario="root";
+    private static String clave="root";
     
+    
+    public static Connection getConexion()
+    {
+        try{
+            Class.forName("");
+        }catch()
+        {
+            
+        }
+        return conex;
+    }
 }
